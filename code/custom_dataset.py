4795cd2e2044416affcb5de053c631ed7ef2c91d
Tai Nguyen <taindp@vf-cbd-qc1-lw.vingroup.local>
2021-07-12 13:38:31 +0700

    att

---
+++ code/custom_dataset.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+class customDataset(Dataset):
+    def __init__(self, X, Y):
+        self.X = X
+        self.y = Y
+        
+    def __len__(self):
+        return len(self.y)
+    
+    def __getitem__(self, idx):
+        return torch.from_numpy(self.X[idx][0].astype(np.float32)), self.y[idx]
---
+++ code/utils.py
@@ -0,0 +1,11 @@
+import torch
+    
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model']
+    model.load_state_dict(checkpoint['state_dict'])
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+
+    model.eval()
+    return model
