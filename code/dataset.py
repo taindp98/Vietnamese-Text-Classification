cfd462ded7b074904e77a05743bda74ea2de9b99
Tai NGUYEN <taindp98@gmail.com>
2023-07-08 21:20:42 +0700

    clean-code

--- code/dataset.py
+++ code/dataset.py
@@ -2,14 +2,15 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+
 class TextDataset(Dataset):
     def __init__(self, X, Y):
         self.X = X
         self.y = Y
-        
+
     def __len__(self):
         return len(self.y)
-    
+
     def __getitem__(self, idx):
         x = self.X[idx][0].astype(np.float32)
         y = self.y[idx]
--- code/utils.py
+++ code/utils.py
@@ -1,9 +1,10 @@
 import torch
-    
+
+
 def load_checkpoint(filepath):
     checkpoint = torch.load(filepath)
-    model = checkpoint['model']
-    model.load_state_dict(checkpoint['state_dict'])
+    model = checkpoint["model"]
+    model.load_state_dict(checkpoint["state_dict"])
     for parameter in model.parameters():
         parameter.requires_grad = False
 
